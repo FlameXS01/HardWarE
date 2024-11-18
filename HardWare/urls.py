a8b844694d7b5805434b52de7d458d6c08910f2d
Jailer <perezjailer41@gmail.com>
2024-11-18 09:00:16 -0500

    changes in the directories

--- HardWare/urls.py
+++ HardWare/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for HardWare_Cimex project.
+URL configuration for HardWare project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- HardWare/wsgi.py
+++ HardWare/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HardWare_Cimex project.
+WSGI config for HardWare project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HardWare_Cimex.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HardWare.settings')
 
 application = get_wsgi_application()
