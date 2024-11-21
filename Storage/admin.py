a23900147f42c08ecadfef45b8da38f548c4ab86
Jailer <perezjailer41@gmail.com>
2024-11-21 10:40:44 -0500

    add models to admin site

--- Storage/admin.py
+++ Storage/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from Storage.models import *
 
 # Register your models here.
+admin.site.register(Chasis)
+admin.site.register(Pc)
+admin.site.register(Lector)
+admin.site.register(Ranura_Expansion)
+admin.site.register(Almacenamiento)
+admin.site.register(Placa_Base)
+admin.site.register(Procesador)
+admin.site.register(Ram)
+admin.site.register(Tarjeta_Red)
+admin.site.register(Fuente)
+admin.site.register(Perifericos)
+admin.site.register(Incidencias)
+
+
+#para salgan hay q en el self ponerle un string o aqui sobreescribir y registrar ___Admin 
