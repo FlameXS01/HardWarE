98edfc2afe19ffe6795e5576087960a13cf99d0d
Jailer <perezjailer41@gmail.com>
2024-11-29 09:30:29 -0500

    zz

--- Storage/tasks.py
+++ Storage/tasks.py
@@ -1,15 +1,12 @@
 from celery import shared_task
 from .models import Trys 
+import os
+import socket 
 
 
 @shared_task
-def mi_tarea():
-    nueva_tarea = Trys(resultado=14)
-    nueva_tarea.save()
-    # Devolver el objeto como un diccionario
-    return {
-        'id_trys': nueva_tarea.id_trys,
-        'resultado': nueva_tarea.resultado,
-        # Agrega otros campos que necesites
-    }
-
+def scan_connected_devices():
+    print('hola')
+    
+    # Procesar y devolver los dispositivos
+    return 1
